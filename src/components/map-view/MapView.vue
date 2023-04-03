976e6eb191e46bf328183e7b37968d5f086cfbe4
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2023-04-03 17:09:55 -0500

    Creo referencia al contenedor del mapa
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/components/map-view/MapView.vue
+++ src/components/map-view/MapView.vue
@@ -1,12 +1,34 @@
 <script lang="ts" src="./map-view"></script>
 
 <template>
-    <h2>MapView</h2>
-    <ul>
-        <li>{{ isLoading }}</li>
-        <li>{{ userLocation }}</li>
-        <li>{{ isUserLocationReady }}</li>
-    </ul>
+    <div v-if="!isUserLocationReady" class="loading-map d-flex justify-content-center align-items-center">
+        <div class="text-center">
+            <h3>Por favor espere</h3>
+            <span>Localizando...</span>
+        </div>
+    </div>
+
+    <div v-show="isUserLocationReady" class="map-container" ref="mapElement">
+
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.loading-map {
+    background-color: rgba(0, 0, 0, 0.8);
+    color: white;
+    height: 100vh;
+    left: 0;
+    position: fixed;
+    top: 0;
+    width: 100vw;
+    z-index: 999;
+}
+
+.map-container {
+    background-color: grey;
+    height: 100vh;
+    position: fixed;
+    width: 100vw;
+}
+</style>
--- src/components/map-view/map-view.ts
+++ src/components/map-view/map-view.ts
@@ -1,14 +1,20 @@
-import { defineComponent } from 'vue'
+import { defineComponent, onMounted, ref } from 'vue'
 
 import { usePlaces } from '@/composables/usePlaces'
 
 export default defineComponent({
     name: 'MapView',
     setup() {
+        const mapElement = ref<HTMLDivElement>()
         const { isLoading, userLocation, isUserLocationReady } = usePlaces()
+
+        onMounted(() => {
+            console.log('mapElement', mapElement.value)
+        })
 
         return {
             isLoading,
+            mapElement,
             userLocation,
             isUserLocationReady,
         }
