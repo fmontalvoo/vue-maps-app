3d117ddcace5f22ac8c393ef72c00fc23b8bf2b6
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2023-04-05 10:24:26 -0500

    Creo store para el mapa
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/components/map-view/map-view.ts
+++ src/components/map-view/map-view.ts
@@ -11,14 +11,29 @@ export default defineComponent({
         const { isLoading, userLocation, isUserLocationReady } = usePlaces()
 
         const initMap = async () => {
+            if (!userLocation.value) throw new Error('No user location')
+
             await Promise.resolve() // wait for the DOM to update
 
             const map = new Mapboxgl.Map({
                 container: mapElement.value!, // container ID
-                style: 'mapbox://styles/mapbox/streets-v12', // style URL
+                style: 'mapbox://styles/mapbox/dark-v10', // style URL
                 center: userLocation.value, // starting position [lng, lat]
                 zoom: 14, // starting zoom
             })
+
+            const popup = new Mapboxgl.Popup({ offset: 25 })
+                .setLngLat(userLocation.value)
+                .setHTML(`
+                <h3>Hola Mundo!</h3>
+                <p>${userLocation.value.join(', ')}</p>
+                `)
+
+            const marker = new Mapboxgl.Marker()
+                .setLngLat(userLocation.value)
+                .setPopup(popup)
+                .addTo(map)
+
         }
 
         onMounted(() => {
---
+++ src/stores/map.ts
@@ -0,0 +1,26 @@
+import { ref, computed } from 'vue'
+
+import { defineStore } from 'pinia'
+
+
+export const useMapStore = defineStore('map', () => {
+    const map = ref<mapboxgl.Map | undefined>()
+    const markers = ref<mapboxgl.Marker[]>([])
+    const distance = ref<number | undefined>()
+    const duration = ref<number | undefined>()
+
+
+    return {
+        // state
+        map,
+        markers,
+        distance,
+        duration,
+        // getters
+        isMapReady: computed(() => !!map.value),
+        // actions
+        setMap(m: mapboxgl.Map) {
+            map.value = m
+        }
+    }
+})
