7a795d1bb65cfecf108a6aca7aa55bea3e8cf51b
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2023-04-05 17:41:46 -0500

    Agrego descripcion para la aplicacion
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,20 @@
 <template>
-  <h2>About</h2>
-</template>
+  <div class="container mt-3">
+    <h1 class="title text-center">Acerca de este proyecto</h1>
+    <div class="mt-5">
+      <p class="text-start" align="justify">
+        El proyecto consiste en desarrollar una aplicación web utilizando Vue 3 y la API de Mapbox, con el fin de ofrecer
+        una experiencia interactiva y personalizada para el usuario al momento de buscar lugares y trazar rutas en un
+        mapa.
+      </p>
 
-<style scoped></style>
+      <p class="text-start" align="justify">
+        La aplicación cuenta con una interfaz de usuario intuitiva y amigable que permite al usuario buscar lugares
+        específicos, ya sea mediante la introducción de la dirección o el nombre del lugar en la barra de búsqueda. Al
+        seleccionar un lugar, el mapa se centrará en la ubicación y se mostrará información detallada sobre el lugar, como
+        su dirección, reseñas de usuarios y fotografías. Además, la aplicación también permitirá al usuario trazar una
+        ruta desde su ubicación actual hasta el lugar seleccionado
+      </p>
+    </div>
+  </div>
+</template>
