549beb3503d53e6093e84daaa3e7eada9db13e29
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2023-04-05 09:40:17 -0500

    Muestro mapa en la aplicacion
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/components/map-view/map-view.ts
+++ src/components/map-view/map-view.ts
@@ -1,4 +1,6 @@
-import { defineComponent, onMounted, ref } from 'vue'
+import { defineComponent, onMounted, ref, watch } from 'vue'
+
+import Mapboxgl from 'mapbox-gl'
 
 import { usePlaces } from '@/composables/usePlaces'
 
@@ -8,8 +10,24 @@ export default defineComponent({
         const mapElement = ref<HTMLDivElement>()
         const { isLoading, userLocation, isUserLocationReady } = usePlaces()
 
+        const initMap = async () => {
+            await Promise.resolve() // wait for the DOM to update
+
+            const map = new Mapboxgl.Map({
+                container: mapElement.value!, // container ID
+                style: 'mapbox://styles/mapbox/streets-v12', // style URL
+                center: userLocation.value, // starting position [lng, lat]
+                zoom: 14, // starting zoom
+            })
+        }
+
         onMounted(() => {
             console.log('mapElement', mapElement.value)
+            if (isUserLocationReady.value) initMap()
+        })
+
+        watch(isUserLocationReady, (isReady) => {
+            if (isReady) initMap()
         })
 
         return {
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import mapboxgl from 'mapbox-gl'
+
 import App from './App.vue'
 import router from './router'
+
+const token = import.meta.env.VITE_MAPBOX_TOKEN
+
+mapboxgl.accessToken = token
 
 const app = createApp(App)
 
